3c53ee7c3e70df8467771dc351d14b5e1f48e559
ferbautistamollo <ferbautistamollo@gmail.com>
2026-09-01 02:31:17 -0400

    archivo para rutas de reportes

---
+++ src/api/reports.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { ResponseData } from "@/utils/interfaces";
+import { apiClient } from "@/services";
+
+export const getForGenerateReport = async (): Promise<ResponseData> => {
+  try {
+    const response = await apiClient.GET(`sales/forGenerateReport`);
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        error: true,
+        message: "Ocurrió un error",
+        data: response.statusText,
+      };
+    }
+
+    return {
+      error: data.error,
+      message: data.message,
+      data: data.data,
+    };
+  } catch (error: any) {
+    return {
+      error: true,
+      message: "Error al obtener datos para la generación de reportes: " + error.message,
+      data: error.message,
+    };
+  }
+};
